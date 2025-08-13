561fff463d166d97bc28e34a0cff428e9295edf8
Harshit Garg <hg1229@gmail.com>
2025-08-13 14:48:36 -0400

    Update cv and resume links

--- src/pages/cv.ts
+++ src/pages/cv.ts
@@ -1,7 +1,7 @@
-import type {APIRoute} from 'astro';
+import type { APIRoute } from 'astro';
 
 // To add external redirects, create a .ts file in the pages/ folder with the route as the name. Then add the following code there.
 
-export const GET: APIRoute = ({redirect}) => {
-    return redirect('https://drive.google.com/file/d/1XB8XGzbEneOgdMDQe3pRDQ3ssGCTpqFi/view?usp=drive_link');;
+export const GET: APIRoute = ({ redirect }) => {
+    return redirect('https://drive.google.com/file/d/1XB8XGzbEneOgdMDQe3pRDQ3ssGCTpqFi/view?usp=sharing');
 }
--- src/pages/resume.ts
+++ src/pages/resume.ts
@@ -1,5 +1,10 @@
-import type {APIRoute} from 'astro';
+import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = ({redirect}) => {
-    return redirect('https://drive.google.com/file/d/19XJ8VN_ZaeN_z6VCbgjEY8hSczKCExhU/view?usp=drive_link');;
-}
+export const GET: APIRoute = ({ redirect }) => {
+    return new Response(null, {
+        status: 302,
+        headers: {
+            Location: 'https://drive.google.com/file/d/19XJ8VN_ZaeN_z6VCbgjEY8hSczKCExhU/view?usp=sharing',
+        },
+    });
+};
